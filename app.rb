ce9660714a2a9b9512271f7a6df51576566ed463
ryan m <contact@ryanmeek.com>
2020-02-23 16:33:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name' { "My name is Mayker" } end
-  get '/hometown' { 'My hometown is Pittsburgh' } end
-  get '/favorite-song' { 'My favorite song is “Flesh and Blood”' } end
+  get('/name') { "My name is Mayker" }
+  get('/hometown') { 'My hometown is Pittsburgh' }
+  get('/favorite-song') { 'My favorite song is "Good Speller, Poor Grammar"' }
 end
