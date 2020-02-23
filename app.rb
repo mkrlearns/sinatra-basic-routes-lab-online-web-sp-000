1caaa9f706c4af70bba7ba2541e81c0b94135f63
ryan m <contact@ryanmeek.com>
2020-02-23 16:32:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' { "My name is Mayker" } end
   get '/hometown' { 'My hometown is Pittsburgh' } end
-  get '/favorite-song' { 'My favorite song is "Folsom Prison Blues"' } end
+  get '/favorite-song' { 'My favorite song is “Flesh and Blood”' } end
 end
